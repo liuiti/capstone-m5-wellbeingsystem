0eed46915e088c685140a562b3a0ede9c25f8041
Pedro <pedro@gmail.com>
2022-07-12 14:30:35 -0300

    Feat: UUID on users and works completed

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,11 @@ from accounts.utils import CustomUserManager
 
 
 class Account(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=50)
     birth_date = models.DateField()
     is_worker = models.BooleanField()
-    # "DD/MM/YYYY"
     username = None
 
     REQUIRED_FIELDS = ["name", "birth_date"]
