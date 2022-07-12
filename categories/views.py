abd9718e20c96ce7f189729b63458740a2e4a8ba
Lorenzo Marques <lorenzo.marques246@gmail.com>
2022-07-12 14:29:34 -0300

    feat:model,serializer & views

---
+++ categories/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import CategorySerializer
+from .mixins import SerializerByMethodMixin
+from .models import Category
+from .permissions import CustomPermission
+
+# Create your views here.
+
+class ListCreateCategoryView(SerializerByMethodMixin, generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, CustomPermission]
+
+    queryset = Category.objects.all()
+    serializer_map = {
+        "GET": CategorySerializer,
+        "POST": CategorySerializer,
+    }
+
+class RetrieveUpdateCategoryView(generics.RetrieveUpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
