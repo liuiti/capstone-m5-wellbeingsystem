ef386c882da7a215e26b0d25eed5c8c41f37d2d6
fabio <fabiofutagawa@gmail.com>
2022-07-19 17:12:39 -0300

    fix: documentation urls fixed

--- WBS/urls.py
+++ WBS/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("api/", include("addresses.urls")),
     path("api/", include("receipts.urls")),
     path("api/", include("avaliations.urls")),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
